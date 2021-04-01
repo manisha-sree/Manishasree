47f39ea76011af1c151bd09ecb9e0e39b036163e
manisha-sree <81732538+manisha-sree@users.noreply.github.com>
2021-04-01 10:30:09 +0530

    Add files via upload

---
+++ mark.java
@@ -0,0 +1,17 @@
+class mark 
+{
+public static void main(String a[])
+{
+int telugu,hindi,english,maths,physics;
+float total,Average,Highest;
+System.out.println("enter the five subjects marks:");
+telugu=100;
+hindi=87;
+english=57;
+maths=75;
+physics=77;
+total=telugu+hindi+english+maths+physics;
+if(telugu>hindi&&telugu>english&&telugu>maths&&telugu>physics)
+{
+System.out.println("highest mark="+telugu);
+
